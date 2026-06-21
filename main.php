e68f6a10ec668bf2646effa11f2612329f749744
unknown <steverezaeipro@gmail.com>
2026-06-21 21:00:17 +0330

    main.php added

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php 
+// $number2 = 10 ;
+// function sum($number){
+//     // global $number2;
+//     // echo $number*$number2;
+//     // echo $number*$GLOBALS['number2'];
+// }
+
+echo "<pre>";
+// var_dump($_SERVER);
+echo $_SERVER['PHP_SELF'];
